7bda49bf0248b9b2af96671dfcab51bd947385db
Miguel Mur Cortés <mmur@ucm.es>
2021-11-02 00:47:18 +0100

    no se si hace falta pero por si acaso

---
+++ PruebaConceptoPractica1/src/TileInfo.java
@@ -0,0 +1,27 @@
+// public class TileInfo {
+//     public TileInfo(boolean isEmpty){
+//         // if(!isEmpty){
+//         //     dirs = new DirInfo[4];
+//         // }
+//     }
+
+//     // public int getDotsSeen() {
+//     //     return _dotsSeen;
+//     // }
+
+//     // public boolean seesEmtpies(){
+//     //     return _seesEmpties;
+//     // }
+
+//     // public void setDotsSeen(int dotsSeen){
+//     //     _dotsSeen = dotsSeen;
+//     // }
+
+//     // public void setSeesEmpties(boolean seesEmpties){
+//     //     _seesEmpties = seesEmpties;
+//     // }
+
+//     public int _dotsSeen = 0;
+//     public boolean _seesEmpties = false;
+//     //private DirInfo[] dirs;
+// }
