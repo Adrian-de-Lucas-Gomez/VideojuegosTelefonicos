4d623d8d1989a646ab8f2c9dd1feb59d89d3aaa9
Tatiana <tatiandu@ucm.es>
2021-11-14 18:06:32 +0100

    mas cmentarios

--- Practica1/engine/src/main/java/com/example/engine/AbstractInput.java
+++ Practica1/engine/src/main/java/com/example/engine/AbstractInput.java
@@ -3,10 +3,15 @@ package com.example.engine;
 import java.util.ArrayList;
 
 public abstract class AbstractInput implements Input {
+    //Lista en la que vamos a guardar los eventos que registremos
     protected ArrayList<TouchEvent> eventList;
+    //Referencia al graphics del motor
     protected Graphics _graphics;
 
-    //Devuelve la lista de eventos
+    /**
+     * Devuelve la lista de eventos
+     * Es sincronizado por si acediesen varias hebras a la vez que vayan de una en una
+     */
     synchronized public ArrayList<TouchEvent> getTouchEvents(){
         if(eventList.size() > 0){
             ArrayList<TouchEvent> auxList = new ArrayList<TouchEvent>(eventList);
