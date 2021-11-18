6d9ae49751bfd5a247976c275ac9b39a08023c59
mmur07 <mmur@ucm.es>
2021-11-18 19:38:33 +0100

    Cositas

---
+++ Practica2_FlowFree/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace flow
+{
+    public class BoardManager : MonoBehaviour
+    {
+        logic.Board board = new logic.Board();
+        void Start()
+        {
+            //Inicializar el tablero
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
