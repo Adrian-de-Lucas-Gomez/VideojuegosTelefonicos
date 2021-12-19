df85675323ef251eaa29fc50359b8a861f15d5d0
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-12-19 20:37:21 +0100

    A tope con el input para varias plataformas

--- Practica2_FlowFree/Assets/Scripts/PlayerInput.cs
+++ Practica2_FlowFree/Assets/Scripts/PlayerInput.cs
@@ -9,38 +9,47 @@ namespace flow
         static public Vector3 GetPointerPosition()
         {
             Vector3 position;
-#if UNITY_ANDROID
-        position = Input.GetTouch(0).position;
-#else //PC & UNITY_EDITOR
+
+#if UNITY_EDITOR
         position = Input.mousePosition;
+#elif UNITY_ANDROID
+        position = Input.GetTouch(0).position;
+#else
+       position = Input.mousePosition;
 #endif
             return Camera.main.ScreenToWorldPoint(position);
         }
 
         static public bool JustPressed()
         {
-#if UNITY_ANDROID
+#if UNITY_EDITOR
+        return Input.GetMouseButtonDown(0);
+#elif UNITY_ANDROID
         return Input.GetTouch(0).phase == TouchPhase.Began;
 #else
-        return Input.GetMouseButtonDown(0);
+       return Input.GetMouseButtonDown(0);
 #endif
         }
 
         static public bool JustUnpressed()
         {
-#if UNITY_ANDROID
-        return Input.GetTouch(0).phase == TouchPhase.Ended;
-#else   
+#if UNITY_EDITOR
         return Input.GetMouseButtonUp(0);
+#elif UNITY_ANDROID
+        return Input.GetTouch(0).phase == TouchPhase.Ended;
+#else
+       return Input.GetMouseButtonUp(0);
 #endif
         }
 
         static public bool IsPressed()
         {
-#if UNITY_ANDROID
-        return Input.GetTouch(0).phase == TouchPhase.Moved;
-#else 
-        return Input.GetMouseButton(0);
+#if UNITY_EDITOR
+       return Input.GetMouseButton(0);
+#elif UNITY_ANDROID
+       return Input.GetTouch(0).phase == TouchPhase.Moved;
+#else
+       return Input.GetMouseButton(0);
 #endif
         }
     }
